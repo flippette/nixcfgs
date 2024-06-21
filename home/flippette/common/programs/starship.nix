7914bc7fd719b5ffedd82e0bb38cca187e3b2a74
flippette <me@flippo.cc>
2024-06-21 20:27:42 +0700

    add starship

---
+++ home/flippette/common/programs/starship.nix
@@ -0,0 +1,19 @@
+{ lib, ... }: {
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      format = lib.concatStrings [
+        "$directory"
+        "$git_branch"
+        "$git_status"
+        "$line_break"
+        "$character"
+      ];
+
+      add_newline = false;
+
+      line_break.disabled = true;
+    };
+  };
+}
