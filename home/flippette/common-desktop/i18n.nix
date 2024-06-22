ff9f017e6009941759554394f099df154f3d22b3
flippette <me@flippo.cc>
2024-06-22 22:58:48 +0700

    re-remove .config/fcitx5/profile :skull:

--- home/flippette/common-desktop/i18n.nix
+++ home/flippette/common-desktop/i18n.nix
@@ -1,33 +1,16 @@
 { pkgs, ... }: {
   # there's not a module for this :pensive:
-  home.file = {
-    ".config/fcitx5/config".text = ''
-      [Hotkey]
-      EnumerateWithTriggerKeys=True
-      EnumerateSkipFirst=False
+  home.file.".config/fcitx5/config".text = ''
+    [Hotkey]
+    EnumerateWithTriggerKeys=True
+    EnumerateSkipFirst=False
 
-      [Hotkey/TriggerKeys]
-      0=Super+BackSpace
+    [Hotkey/TriggerKeys]
+    0=Super+BackSpace
 
-      [Behavior]
-      ShareInputState=All
-    '';
-    ".config/fcitx5/profile".text = ''
-      [Groups/0]
-      Name=Default
-      Default Layout=us
-      DefaultIM=unikey
-
-      [Groups/0/Items/0]
-      Name=keyboard-us
-
-      [Groups/0/Items/1]
-      Name=unikey
-
-      [GroupOrder]
-      0=Default
-    '';
-  };
+    [Behavior]
+    ShareInputState=All
+  '';
 
   i18n.inputMethod = {
     enabled = "fcitx5";
