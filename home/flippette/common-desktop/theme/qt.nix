16c7826b414b1371bef84ad89e827bf568ec55f4
flippette <me@flippo.cc>
2024-06-22 23:46:25 +0700

    add lightly-qt

--- home/flippette/common-desktop/theme/qt.nix
+++ home/flippette/common-desktop/theme/qt.nix
@@ -1,23 +1,29 @@
 { config, pkgs, ... }: {
+  home.packages = [
+    pkgs.catppuccin-qt5ct
+    pkgs.lightly-qt
+  ];
+
   qt = {
     enable = true;
     platformTheme.name = "qtct";
-    style = {
-      name = "Catppuccin-Mocha";
-      package = pkgs.catppuccin-qt5ct;
-    };
   };
 
   xdg.configFile =
   let
-    package = config.qt.style.package;
-    name = config.qt.style.name;
+    package = pkgs.catppuccin-qt5ct;
+    name = "Catppuccin-Mocha";
     themeDir = "${package}/share/qt5ct/colors";
     text = ''
       [Appearance]
       custom_palette=true
       color_scheme_path=${themeDir}/${name}.conf
       standard_dialogs=xdgdesktopportal
+      style=Lightly
+
+      [Fonts]
+      fixed="Cantarell,12,-1,5,400,0,0,0,0,0,0,0,0,0,0,1,Regular"
+      general="Cantarell,12,-1,5,400,0,0,0,0,0,0,0,0,0,0,1,Regular"
     '';
   in {
     "qt5ct/qt5ct.conf".text = text;
