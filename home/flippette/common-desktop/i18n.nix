ed33b7ecfa00a9cd591fe472b6543b0c0b796b8f
flippette <me@flippo.cc>
2024-06-22 22:44:59 +0700

    re-add .config/fcitx5/profile

--- home/flippette/common-desktop/i18n.nix
+++ home/flippette/common-desktop/i18n.nix
@@ -1,19 +1,37 @@
 { pkgs, ... }: {
   # there's not a module for this :pensive:
-  home.file.".config/fcitx5/config".text = ''
-    [Hotkey]
-    EnumerateWithTriggerKeys=True
-    EnumerateSkipFirst=False
+  home.file = {
+    ".config/fcitx5/config".text = ''
+      [Hotkey]
+      EnumerateWithTriggerKeys=True
+      EnumerateSkipFirst=False
 
-    [Hotkey/TriggerKeys]
-    0=Super+BackSpace
+      [Hotkey/TriggerKeys]
+      0=Super+BackSpace
 
-    [Behavior]
-    ShareInputState=All
-  '';
+      [Behavior]
+      ShareInputState=All
+    '';
+    ".config/fcitx5/profile".text = ''
+      [Groups/0]
+      Name=Default
+      Default Layout=us
+      DefaultIM=unikey
+
+      [Groups/0/Items/0]
+      Name=keyboard-us
+
+      [Groups/0/Items/1]
+      Name=unikey
+
+      [GroupOrder]
+      0=Default
+    '';
+  };
 
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = [ pkgs.fcitx5-unikey ];
   };
 }
+
