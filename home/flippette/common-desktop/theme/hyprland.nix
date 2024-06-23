de60d1da42b32a44bf53897fc2b33f7ab16ca304
flippette <me@flippo.cc>
2024-06-23 17:57:05 +0700

    fix hyprland and waybar theming

--- home/flippette/common-desktop/theme/hyprland.nix
+++ home/flippette/common-desktop/theme/hyprland.nix
@@ -1,10 +1,11 @@
-{ config, ... }: {
-  let
-    themeFile = builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/catppuccin/hyprland/main/themes/mocha.conf";
-      sha256 = "4b154dbd96637ee3c0db207dc40d041c712713d788409005541214b838922314";
-    };
-  in wayland.windowManager.hyprland = {
+{ config, ... }:
+let
+  themeFile = builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/catppuccin/hyprland/main/themes/mocha.conf";
+    sha256 = "4b154dbd96637ee3c0db207dc40d041c712713d788409005541214b838922314";
+  };
+in {
+  wayland.windowManager.hyprland = {
     settings = {
       source = [ "${config.xdg.configHome}/hypr/theme.conf" ];
       general."col.active_border" = "$mauve";
