d8eb854279c53ed5564d3c9b6527f5c1c951a814
flippette <me@flippo.cc>
2024-06-23 17:54:35 +0700

    simplify hyprland theming

--- home/flippette/common-desktop/theme/hyprland.nix
+++ home/flippette/common-desktop/theme/hyprland.nix
@@ -1,11 +1,10 @@
 { config, ... }: {
-  # theme file
-  xdg.configFile."hypr/theme.conf".source = builtins.fetchurl {
-    url = "https://raw.githubusercontent.com/catppuccin/hyprland/main/themes/mocha.conf";
-    sha256 = "4b154dbd96637ee3c0db207dc40d041c712713d788409005541214b838922314";
-  };
-
-  wayland.windowManager.hyprland = {
+  let
+    themeFile = builtins.fetchurl {
+      url = "https://raw.githubusercontent.com/catppuccin/hyprland/main/themes/mocha.conf";
+      sha256 = "4b154dbd96637ee3c0db207dc40d041c712713d788409005541214b838922314";
+    };
+  in wayland.windowManager.hyprland = {
     settings = {
       source = [ "${config.xdg.configHome}/hypr/theme.conf" ];
       general."col.active_border" = "$mauve";
