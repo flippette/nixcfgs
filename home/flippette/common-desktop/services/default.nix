a8c4583604d017c55708a6301b2e00cd1adba988
flippette <me@flippo.cc>
2024-06-24 01:47:36 +0700

    add hyprpaper

--- home/flippette/common-desktop/services/default.nix
+++ home/flippette/common-desktop/services/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./blueman.nix
     ./dunst.nix
+    ./hyprpaper.nix
     ./networkmanager.nix
     ./udiskie.nix
   ];
---
+++ home/flippette/common-desktop/services/hyprpaper.nix
@@ -0,0 +1,16 @@
+{ ... }:
+let
+  wallpaper = builtins.fetchurl {
+    url = "https://img3.gelbooru.com/images/08/46/0846e88933a1c85c59a8ccd640f1e0c0.png";
+    sha256 = "30c9a69cc672b5cfd781f1f1a1be17d043b551e17c6ea2cf4c015c9c9a94fdc5";
+  };
+in {
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      preload = "${wallpaper}";
+      wallpaper = ",${wallpaper}";
+      splash = false;
+    };
+  };
+}
