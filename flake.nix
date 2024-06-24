e5420e8634a9b7b677fef076a995d225ea9ba029
flippette <me@flippo.cc>
2024-06-25 02:57:30 +0700

    add formatter

--- flake.nix
+++ flake.nix
@@ -1,3 +1,11 @@
 {
-  outputs = inputs: {};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ nixpkgs, ... }: {
+    formatter = with nixpkgs.legacyPackages; {
+      x86_64-linux = x86_64-linux.nixfmt-rfc-style;
+    };
+  };
 }
