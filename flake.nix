7efbb147cf88b6d243d2a0a00bf0fdff4cafd1c2
flippette <me@flippo.cc>
2024-06-25 05:31:22 +0700

    update flake.nix to use new home and opts

--- flake.nix
+++ flake.nix
@@ -1,10 +1,15 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    inputs@{ nixpkgs, ... }:
+    inputs@{ nixpkgs, home-manager, ... }:
     {
       formatter = with nixpkgs.legacyPackages; {
         x86_64-linux = x86_64-linux.nixfmt-rfc-style;
@@ -17,6 +22,18 @@
             ./sys/oyasumi
             ./theme/catppuccin-mocha/sys
             { system.stateVersion = "24.05"; }
+
+            home-manager.nixosModules.home-manager 
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.flippette.imports = [
+                ./home
+                ./theme/catppuccin-mocha/home
+                { home.stateVersion = "24.05"; }
+              ];
+              home-manager.extraSpecialArgs = { inherit inputs; };
+            }
           ];
         };
       };
