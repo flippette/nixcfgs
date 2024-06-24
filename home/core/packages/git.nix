1124186e4946661d447d4b444dfa3b331ab7b3a2
flippette <me@flippo.cc>
2024-06-25 06:57:04 +0700

    extra git configs

--- home/core/packages/git.nix
+++ home/core/packages/git.nix
@@ -1,6 +1,13 @@
 {
   programs.git = {
+    config = {
+      init.defaultBranch = "main";
+      core.autocrlf = false;
+      color.ui = true;
+    };
+
     enable = true;
+    lfs.enable = true;
     userEmail = "me@flippo.cc";
     userName = "flippette";
   };
