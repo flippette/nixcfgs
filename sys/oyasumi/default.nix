0851721ea10e39703e0aa78c12468815873c79dc
flippette <me@flippo.cc>
2024-06-25 05:38:07 +0700

    fix opts reference in sys/oyasumi (again)

--- sys/oyasumi/default.nix
+++ sys/oyasumi/default.nix
@@ -1,8 +1,8 @@
 {
   imports = [
-    ../../opts/amd_cpu.nix
-    ../../opts/amd_gpu.nix
-    ../../opts/cpu_gov_perf.nix
+    ../../opts/sys/amd_cpu.nix
+    ../../opts/sys/amd_gpu.nix
+    ../../opts/sys/cpu_gov_perf.nix
     ../core
     ./filesystems.nix
     ./hardware-configuration.nix
