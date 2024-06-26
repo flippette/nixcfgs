e3d9a4c9808f01659904a8204c5aec6cda4b678c
flippette <me@flippo.cc>
2024-06-26 12:46:30 +0700

    enable quiet boot for plymouth theming

--- theme/catppuccin-mocha/sys/plymouth.nix
+++ theme/catppuccin-mocha/sys/plymouth.nix
@@ -1,8 +1,24 @@
 { pkgs, ... }:
 {
-  boot.plymouth = {
-    enable = true;
-    theme = "catppuccin-mocha";
-    themePackages = [ (pkgs.catppuccin-plymouth.override { variant = "mocha"; }) ];
+  boot = {
+    # quiet boot
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    
+    # quiet boot
+    kernelParams = [
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "quiet"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+
+    plymouth = {
+      enable = true;
+      theme = "catppuccin-mocha";
+      themePackages = [ (pkgs.catppuccin-plymouth.override { variant = "mocha"; }) ];
+    };
   };
 }
