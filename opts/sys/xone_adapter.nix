87560def87979d84f7b9293a13846adee9abde04
flippette <me@flippo.cc>
2024-06-26 13:09:00 +0700

    add xow_dongle-firmware for opts/sys/xone_adapter.nix

--- opts/sys/xone_adapter.nix
+++ opts/sys/xone_adapter.nix
@@ -1,7 +1,9 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   boot = {
     blacklistedKernelModules = [ "mt76x2u" ];
-    extraModulePackages = with config.boot.kernelPackages; [ xone ];
+    extraModulePackages = [ config.boot.kernelPackages.xone ];
   };
+
+  environment.systemPackages = [ pkgs.xow_dongle-firmware ];
 }
